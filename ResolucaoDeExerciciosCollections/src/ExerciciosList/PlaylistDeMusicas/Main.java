38ee78f1948d2c145b46892e9dd4744f425555de
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-01 13:41:55 -0300

    Resolução do exercício Playlist de músicas

--- ResolucaoDeExerciciosCollections/src/ExerciciosList/PlaylistDeMusicas/Main.java
+++ ResolucaoDeExerciciosCollections/src/ExerciciosList/PlaylistDeMusicas/Main.java
@@ -1,4 +1,62 @@
 package ExerciciosList.PlaylistDeMusicas;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Playlist playlist = new Playlist();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem vindo a sua playlist");
+        boolean sair = false;
+        while(sair != true){
+            System.out.println("\t\tDigite a opcao desejada.");
+            System.out.println("\t1 - Adicionar uma música na playlist: ");
+            System.out.println("\t2 - Remover uma música da playlist: ");
+            System.out.println("\t3 - Saber a duração total da playlist: ");
+            System.out.println("\t4 - Exibir todas as músicas da playlist: ");
+            System.out.println("\t0 - Sair");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+            switch(opcao){
+                case 1:
+                    System.out.println("\tAdicionar nova música");
+                    System.out.println("Digite o nome da música: ");
+                    String nome = scanner.nextLine();
+                    System.out.println("Digite o nome do artista: ");
+                    String artista = scanner.nextLine();
+                    System.out.println("Digite a duração da música (em segundos): ");
+                    int duracao = scanner.nextInt();
+                    scanner.nextLine();
+                    playlist.adicionarMusica(nome, artista, duracao);
+                    System.out.println("\n");
+                    break;
+                case 2:
+                    System.out.println("\tRemover música");
+                    System.out.println("Digite o nome da música que será removida: ");
+                    String nomeDaMusica = scanner.nextLine();
+                    playlist.removerMusica(nomeDaMusica);
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    System.out.println("\tDuração total da playlist");
+                    System.out.println("A playlist possui a duração de: " + playlist.calcularDuracaoTotal() + " segundos");
+                    System.out.println("\n");
+                    break;
+                case 4:
+                    System.out.println("\tMúsicas na playlist");
+                    playlist.exibirMusicas();
+                    System.out.println("\n");
+                    break;
+                case 0:
+                    sair = true;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+        System.out.println("Sistema encerrado!");
+    }
 }
--- ResolucaoDeExerciciosCollections/src/ExerciciosList/PlaylistDeMusicas/Playlist.java
+++ ResolucaoDeExerciciosCollections/src/ExerciciosList/PlaylistDeMusicas/Playlist.java
@@ -1,4 +1,53 @@
 package ExerciciosList.PlaylistDeMusicas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
+    private List<Musica> musicaList;
+
+    public Playlist() {
+        this.musicaList = new ArrayList<>();
+    }
+
+    public void adicionarMusica(String nome, String artista, int duracao) {
+        musicaList.add(new Musica(nome, artista, duracao));
+    }
+
+    public void removerMusica(String nome) {
+        List<Musica> musicaRemovida = new ArrayList<>();
+        if(!musicaList.isEmpty()){
+            for(Musica musicas : musicaList) {
+                if(musicas.getNome().equalsIgnoreCase(nome)){
+                    musicaRemovida.add(musicas);
+                    break;
+                }
+            }
+            musicaList.removeAll(musicaRemovida);
+        }else{
+            System.out.println("A Playlist está vazia!");
+        }
+    }
+
+    public int calcularDuracaoTotal() {
+        int duracao = 0;
+        if(!musicaList.isEmpty()){
+            for(Musica musicas : musicaList){
+                duracao += musicas.getDuracao();
+            }
+            return duracao;
+        }else{
+            throw new RuntimeException("A Playlist está vazia!");
+        }
+    }
+
+    public void exibirMusicas() {
+        if(!musicaList.isEmpty()){
+            for(Musica musica : musicaList){
+                System.out.println(musica);
+            }
+        }else{
+            System.out.println("A playlist está vazia!");
+        }
+    }
 }
