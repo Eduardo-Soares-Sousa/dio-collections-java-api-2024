4aa34ab6be1ecff5feffd596936aa319917bdc2f
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-02 16:52:41 -0300

    Resolução do exercício Agenda contatos

--- ResolucaoDeExerciciosCollections/src/ExerciciosList/AgendaDeContatos/Agenda.java
+++ ResolucaoDeExerciciosCollections/src/ExerciciosList/AgendaDeContatos/Agenda.java
@@ -1,4 +1,55 @@
 package ExerciciosList.AgendaDeContatos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Agenda {
+    private List<Contato> contatoList;
+
+    public Agenda() {
+        this.contatoList = new ArrayList<>();
+    }
+
+    public void adicionarContato(String nome, String telefone, String email) {
+        contatoList.add(new Contato(nome, telefone, email));
+    }
+
+    public void removerContato(String nome) {
+        List<Contato> contatoRemover = new ArrayList<>();
+        if(!contatoList.isEmpty()){
+            for(Contato contato : contatoList){
+                if(contato.getNome().equalsIgnoreCase(nome)){
+                    contatoRemover.add(contato);
+                    break;
+                }
+            }
+            contatoList.removeAll(contatoRemover);
+        }else{
+            System.out.println("A lista de contatos está vazia!");
+        }
+    }
+
+    public void atualizarEmailContato(String nome, String novoEmail) {
+        if(!contatoList.isEmpty()){
+            for(Contato contato : contatoList){
+                if(contato.getNome().equalsIgnoreCase(nome)){
+                    contato.setEmail(novoEmail);
+                    break;
+                }
+            }
+        }else{
+            System.out.println("A lista de contatos está vazia!");
+        }
+    }
+
+    public void exibirContatos(){
+        Set<Contato> exibirContatos = new TreeSet<>(contatoList);
+        if(!contatoList.isEmpty()){
+            System.out.println(exibirContatos);
+        }else{
+            System.out.println("A lista de contatos está vazia!");
+        }
+    }
 }
--- ResolucaoDeExerciciosCollections/src/ExerciciosList/AgendaDeContatos/Main.java
+++ ResolucaoDeExerciciosCollections/src/ExerciciosList/AgendaDeContatos/Main.java
@@ -1,4 +1,23 @@
 package ExerciciosList.AgendaDeContatos;
 
 public class Main {
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+
+        agenda.adicionarContato("Maria", "1111111", "mariaEmail");
+        agenda.adicionarContato("Roberto", "2222222", "robertoEmail");
+        agenda.adicionarContato("rosita", "3333333", "rositaEmail");
+        agenda.adicionarContato("Cleide", "4444444", "cleideEmail");
+        agenda.adicionarContato("José", "5555555", "joséEmail");
+
+        agenda.exibirContatos();
+
+        agenda.removerContato("rosita");
+        agenda.removerContato("Maria");
+
+        agenda.exibirContatos();
+
+        agenda.atualizarEmailContato("Roberto", "RobertsEmail.outlook");
+        agenda.exibirContatos();
+    }
 }
