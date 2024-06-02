f4c88a4b5721fa6bea3aaf86fa887fe9ea8ace19
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-02 15:04:50 -0300

    Resolção do exercício Registro de alunos

--- ResolucaoDeExerciciosCollections/src/ExerciciosList/RegistroDeAlunos/Main.java
+++ ResolucaoDeExerciciosCollections/src/ExerciciosList/RegistroDeAlunos/Main.java
@@ -1,4 +1,32 @@
 package ExerciciosList.RegistroDeAlunos;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        RegistroAluno registroAluno = new RegistroAluno();
+
+        registroAluno.adicionarAluno("Gabriel", 10);
+        registroAluno.adicionarAluno("Jennifer", 10);
+        registroAluno.adicionarAluno("Matheus", 10);
+        registroAluno.adicionarAluno("Isabela", 10);
+        registroAluno.adicionarAluno("João", 10);
+
+        registroAluno.obterNomesAlunos();
+        List<String> alunos =  registroAluno.obterNomesAlunos();
+        for(String a : alunos){
+            System.out.println(a);
+        }
+
+        System.out.println("Há " + registroAluno.obterNumeroTotalAlunos() + " alunos registrados");
+
+        registroAluno.removerAluno("Matheus");
+
+        System.out.println("Há " + registroAluno.obterNumeroTotalAlunos() + " alunos registrados");
+
+        List<String> aluno =  registroAluno.obterNomesAlunos();
+        for(String a : aluno){
+            System.out.println(a);
+        }
+    }
 }
--- ResolucaoDeExerciciosCollections/src/ExerciciosList/RegistroDeAlunos/RegistroAluno.java
+++ ResolucaoDeExerciciosCollections/src/ExerciciosList/RegistroDeAlunos/RegistroAluno.java
@@ -1,4 +1,48 @@
 package ExerciciosList.RegistroDeAlunos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegistroAluno {
+    private List<Aluno> alunoList;
+
+    public RegistroAluno() {
+        this.alunoList = new ArrayList<>();
+    }
+
+    public void adicionarAluno(String nome, int idade) {
+        alunoList.add(new Aluno(nome, idade));
+    }
+
+    public void removerAluno(String nome) {
+        List<Aluno> alunoRemover = new ArrayList<>();
+        if(!alunoList.isEmpty()){
+            for(Aluno aluno : alunoList){
+                if(aluno.getNome().equalsIgnoreCase(nome)){
+                    alunoRemover.add(aluno);
+                    break;
+                }
+            }
+            alunoList.removeAll(alunoRemover);
+        }else{
+            System.out.println("Não há alunos no registro!");
+        }
+    }
+
+    public int obterNumeroTotalAlunos() {
+        int totalAluno = alunoList.size();
+        return totalAluno;
+    }
+
+    public List<String> obterNomesAlunos() {
+        List<String> obterAlunosNome = new ArrayList<>();
+        if(!alunoList.isEmpty()){
+            for(Aluno aluno : alunoList){
+                obterAlunosNome.add(aluno.getNome());
+            }
+            return obterAlunosNome;
+        }else{
+            throw new RuntimeException("A lista de alunos está vazia!");
+        }
+    }
 }
